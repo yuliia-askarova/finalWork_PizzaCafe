59716a8a1b2b8f1a7b1795e13f594f981a0b07c4
yuliia-askarova <lastsong428@gmail.com>
2024-03-29 11:50:53 +0000

    second commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter, RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+
+  
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
